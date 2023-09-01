4e73072e9e4413d1bcc8ebad0081918ac65ca2be
danielasofiassis <daniela.sf.assis@gmail.com>
2023-09-01 16:12:06 -0300

    new commit

---
+++ pyhton/tipos_de_operadores.py
@@ -0,0 +1,30 @@
+# adição
+print (1 + 1)
+
+# subtração
+print (1 - 1)
+
+# multiplicação
+print (1 * 1)
+
+# divisão
+print (1 / 1) # número float
+
+# divisão inteira
+print (1 // 1)
+
+# módulo
+print (1 % 1)
+
+# exponenciação 
+print (1 ** 1)
+
+# O PYTHON SEGUE A PRECEDÊNCIA DA MATEMÁTICA DE ORDEM DAS OPERAÇÕES
+
+
+
+
+
+
+
+
