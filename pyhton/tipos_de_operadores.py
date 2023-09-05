57eee68a4be7c0263ff48e6c2019470c6f750cc0
danielasofiassis <daniela.sf.assis@gmail.com>
2023-09-05 15:51:20 -0300

    novo commit

--- pyhton/tipos_de_operadores.py
+++ pyhton/tipos_de_operadores.py
@@ -1,3 +1,5 @@
+# OPERADORES ARITMÉTICOS
+
 # adição
 print (1 + 1)
 
@@ -19,9 +21,60 @@ print (1 % 1)
 # exponenciação 
 print (1 ** 1)
 
-# O PYTHON SEGUE A PRECEDÊNCIA DA MATEMÁTICA DE ORDEM DAS OPERAÇÕES
+produto_1 = 10
+produto_2 =  20
+
+print(produto_1+produto_2)
+print(produto_1-produto_2)
+print(produto_1/produto_2)
+print(produto_1//produto_2)
+print(produto_1%produto_2)
+print(produto_1**produto_2)
+
+#PYTHON SEGUE A PRECEDÊNCIA DA MATEMÁTICA DE ORDEM DAS OPERAÇÕES:
+x = 10-5*2
+y = 10/0.5 + ((2-1) ** 4)
+print(x)
+print(y)
+
+# OPERADORES DE COMPARAÇÃO : resultado é bool (V/F)
+# igualdade ==
+# diferença !=
+# maior ou igual é > OU >=
+# menor ou igual é < OU <=
+
+saldo = 200
+saque = 200
+print(saldo==saque)
+print(saldo!= saque)
+print(saldo>=saque)
+print(saldo>saque)
+print(saldo<=saque)
+print(saldo<saque)
 
 
+# OPERADORES DE ATRIBUIÇÃO : define valor inicial ou sobrescrever valor de variável
+saldo = 500 #atribui 500 a variável saldo
+saldo += 200 #atribui com soma no valor da variável
+saldo -= 200 #atribui com subtração no valor da variável
+saldo *= 200 #atribui com multiplicação no valor da variável
+saldo /= 200 #atribui com divisão no valor da variável
+saldo //= 200 #atribui com divisão no valor da variável
+saldo %= 200 #atribui com soma no valor da variável
+saldo **= 200 #atribui com soma no valor da variável
+
+
+# OPERADORES LÓGICOS : resultado booleano 
+saldo = 1000
+saque = 200
+limite = 100
+
+saldo >= saque and saque <= limite #AND 
+
+
+# OPERADORES DE IDENTIDADE
+
+# OPERADORES DE ASSOCIAÇÃO
 
 
 
