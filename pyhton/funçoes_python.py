70b33b79a901fc58a711cb52d2d2a90aa795e91a
Daniela Sofia F Assis <danielasofiassis@gmail.com>
2023-10-10 16:58:35 -0300

    funcoes

---
+++ pyhton/funçoes_python.py
@@ -0,0 +1,24 @@
+# FUNÇÕES são blocos de código que tornam o código mais legível
+
+# DECLARAR FUNÇÃO
+# def nome do identificador da função(argumento):
+#   print()
+
+# CHAMAR FUNÇÃO
+# nome do identificador da função(argumento)
+
+#*arg renderiza como tupla
+#*kwargs renderiza como dicionário
+
+# PARAMETROS APENAS POR POSIÇÃO
+# def f(pos 1, pos 2, /, pos_or_kwd):
+#   print()
+
+# PARAMETROS APENAS POR NOME
+# def f(*, kwd1, kwd2, ...):
+#   print()
+
+# PARAMETROS POR NOME OU POSIÇÃO
+# def f(pos 1, pos 2, /, pos_or_kwd):
+#   print()
+
